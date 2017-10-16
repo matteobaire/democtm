0da796e3f39eba9df2045845db3ee78eecabae81
Matteo Baire <mbdigital@gmail.com>
2017-10-16 11:21:39 +0200

    minor tweeks

--- palinsesto.sh
+++ palinsesto.sh
@@ -3,4 +3,4 @@
 # get rid of the cursor so we don't see it when videos are running
 setterm -cursor off
 
-/usr/bin/chromium-browser --kiosk videos.html
+/usr/bin/chromium-browser --disable-web-security --user-data-dir --kiosk videos.html
